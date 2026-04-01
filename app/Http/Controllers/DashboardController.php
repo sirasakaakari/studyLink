6e0eb3458cc3b4047204abe55b733754566ec1f4
shiro <akatashi21@gmail.com>
2026-04-01 17:35:16 +0900

    dd($) 変数の中身表示

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -20,6 +20,7 @@ class DashboardController extends Controller
             ->inRandomOrder()
             ->take(5)
             ->get();
+        dd($recommendedUsers); //
 
         $goals = Goal::where('user_id', Auth::id())->get();
 
