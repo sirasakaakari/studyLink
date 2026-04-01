60ebfa8c0cc4d9bdcc479b608817a9365dc9b446
shiro <akatashi21@gmail.com>
2026-04-01 15:57:05 +0900

    fix: AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
