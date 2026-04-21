0fb3a254275e5cb0575a9ecbe99a25aa2a37c43c
shiro <akatashi21@gmail.com>
2026-04-21 17:35:31 +0900

    create: TrustProxies.php

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * Trust all proxies (Render / Cloud環境対応)
+     */
+    protected $proxies = '*';
+
+    /**
+     * 重要: HTTPS判定を正しくする
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO;
+}
